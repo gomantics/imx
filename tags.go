a3f8936c93998fb6bd92fc40abbe7895ba195ee5
Puneet Rai <puneetrai04@gmail.com>
2025-12-07 12:27:28 +0530

    fix: correct tag IDs to match actual parser output
    IPTC: Use exact dataset names from datasets.go (e.g., Province-State,
    Caption-Abstract, Writer-Editor, OriginalTransmissionReference)
    
    ICC: Use header field names and human-readable tag names with spaces
    from knownTags (e.g., 'Profile Description', 'Media White Point')

--- tags.go
+++ tags.go
@@ -167,18 +167,20 @@ const (
 )
 
 // =============================================================================
-// IPTC Tags
+// IPTC Tags (from datasets.go)
 // =============================================================================
 
 // IPTC Core Identification Tags
 const (
-	TagIPTCObjectName    TagID = "IPTC:ObjectName"    // Title/headline reference
-	TagIPTCUrgency       TagID = "IPTC:Urgency"       // 1=most urgent, 8=least
-	TagIPTCCategory      TagID = "IPTC:Category"      // Subject category code
-	TagIPTCKeywords      TagID = "IPTC:Keywords"      // Keywords for indexing
-	TagIPTCFixtureID     TagID = "IPTC:FixtureID"     // Identifies recurring events
-	TagIPTCEditStatus    TagID = "IPTC:EditStatus"    // Status of object
-	TagIPTCSpecialInstr  TagID = "IPTC:SpecialInstr"  // Special instructions
+	TagIPTCObjectName           TagID = "IPTC:ObjectName"           // Title/shorthand reference
+	TagIPTCUrgency              TagID = "IPTC:Urgency"              // 1=most urgent, 8=least
+	TagIPTCCategory             TagID = "IPTC:Category"             // Subject category code
+	TagIPTCSupplementalCategories TagID = "IPTC:SupplementalCategories" // Additional categories
+	TagIPTCKeywords             TagID = "IPTC:Keywords"             // Keywords for indexing
+	TagIPTCFixtureIdentifier    TagID = "IPTC:FixtureIdentifier"    // Identifies recurring events
+	TagIPTCEditStatus           TagID = "IPTC:EditStatus"           // Status of object
+	TagIPTCSpecialInstructions  TagID = "IPTC:SpecialInstructions"  // Special instructions
+	TagIPTCSubjectReference     TagID = "IPTC:SubjectReference"     // Structured subject reference
 )
 
 // IPTC Date/Time Tags
@@ -190,6 +192,7 @@ const (
 	TagIPTCReleaseDate        TagID = "IPTC:ReleaseDate"        // Earliest release date
 	TagIPTCReleaseTime        TagID = "IPTC:ReleaseTime"        // Earliest release time
 	TagIPTCExpirationDate     TagID = "IPTC:ExpirationDate"     // Latest use date
+	TagIPTCExpirationTime     TagID = "IPTC:ExpirationTime"     // Latest use time
 )
 
 // IPTC Creator/Author Tags
@@ -200,27 +203,27 @@ const (
 	TagIPTCSource         TagID = "IPTC:Source"         // Original owner/creator
 	TagIPTCCopyrightNotice TagID = "IPTC:CopyrightNotice" // Copyright notice
 	TagIPTCContact        TagID = "IPTC:Contact"        // Contact information
-	TagIPTCWriterEditor   TagID = "IPTC:WriterEditor"   // Caption writer
+	TagIPTCWriterEditor   TagID = "IPTC:Writer-Editor"  // Caption writer name
 )
 
 // IPTC Location Tags
 const (
-	TagIPTCCity            TagID = "IPTC:City"            // City of origin
-	TagIPTCSublocation     TagID = "IPTC:Sublocation"     // Location within city
-	TagIPTCProvinceState   TagID = "IPTC:ProvinceState"   // Province/State
-	TagIPTCCountryCode     TagID = "IPTC:CountryCode"     // ISO 3166 country code
-	TagIPTCCountryName     TagID = "IPTC:CountryName"     // Full country name
-	TagIPTCContentLocCode  TagID = "IPTC:ContentLocCode"  // Content location code
-	TagIPTCContentLocName  TagID = "IPTC:ContentLocName"  // Content location name
+	TagIPTCCity                    TagID = "IPTC:City"                       // City of origin
+	TagIPTCSublocation             TagID = "IPTC:Sublocation"                // Location within city
+	TagIPTCProvinceState           TagID = "IPTC:Province-State"             // Province/State of origin
+	TagIPTCCountryCode             TagID = "IPTC:Country-PrimaryLocationCode" // ISO 3166 country code
+	TagIPTCCountryName             TagID = "IPTC:Country-PrimaryLocationName" // Full country name
+	TagIPTCContentLocationCode     TagID = "IPTC:ContentLocationCode"        // Content location code
+	TagIPTCContentLocationName     TagID = "IPTC:ContentLocationName"        // Content location name
 )
 
 // IPTC Description Tags
 const (
-	TagIPTCHeadline       TagID = "IPTC:Headline"       // Publishable headline
-	TagIPTCCaptionAbstract TagID = "IPTC:CaptionAbstract" // Description/caption
-	TagIPTCOriginProgram  TagID = "IPTC:OriginProgram"  // Program that created file
-	TagIPTCProgramVersion TagID = "IPTC:ProgramVersion" // Version of program
-	TagIPTCTransmissionRef TagID = "IPTC:TransmissionRef" // Original reference/job ID
+	TagIPTCHeadline                    TagID = "IPTC:Headline"                    // Publishable headline
+	TagIPTCCaptionAbstract             TagID = "IPTC:Caption-Abstract"            // Description/caption
+	TagIPTCOriginatingProgram          TagID = "IPTC:OriginatingProgram"          // Program that created file
+	TagIPTCProgramVersion              TagID = "IPTC:ProgramVersion"              // Version of program
+	TagIPTCOriginalTransmissionReference TagID = "IPTC:OriginalTransmissionReference" // Original reference/job ID
 )
 
 // =============================================================================
@@ -306,45 +309,59 @@ const (
 )
 
 // =============================================================================
-// ICC Tags
+// ICC Tags (from icc.go header fields and tags.go knownTags)
 // =============================================================================
 
-// ICC Profile Header Tags
+// ICC Profile Header Tags (from icc.go addTag calls)
 const (
-	TagICCProfileDescription TagID = "ICC:ProfileDescription" // Profile description
-	TagICCCopyright          TagID = "ICC:Copyright"          // Copyright notice
+	TagICCProfileSize        TagID = "ICC:ProfileSize"        // Profile size in bytes
+	TagICCPreferredCMM       TagID = "ICC:PreferredCMM"       // Preferred CMM
+	TagICCVersion            TagID = "ICC:Version"            // Profile version
 	TagICCProfileClass       TagID = "ICC:ProfileClass"       // Profile class
 	TagICCColorSpace         TagID = "ICC:ColorSpace"         // Color space
 	TagICCPCS                TagID = "ICC:PCS"                // Profile connection space
-	TagICCProfileVersion     TagID = "ICC:ProfileVersion"     // Profile version
+	TagICCCreateDate         TagID = "ICC:CreateDate"         // Profile creation date
+	TagICCPlatform           TagID = "ICC:Platform"           // Primary platform
+	TagICCRenderingIntent    TagID = "ICC:RenderingIntent"    // Rendering intent
 	TagICCDeviceManufacturer TagID = "ICC:DeviceManufacturer" // Device manufacturer
 	TagICCDeviceModel        TagID = "ICC:DeviceModel"        // Device model
-	TagICCRenderingIntent    TagID = "ICC:RenderingIntent"    // Rendering intent
-	TagICCCreationDate       TagID = "ICC:CreationDate"       // Profile creation date
-	TagICCPlatform           TagID = "ICC:Platform"           // Primary platform
-	TagICCCMMType            TagID = "ICC:CMMType"            // CMM type
+	TagICCCreator            TagID = "ICC:Creator"            // Profile creator
+	TagICCPCSIlluminant      TagID = "ICC:PCSIlluminant"      // PCS illuminant
+	TagICCProfileFlags       TagID = "ICC:ProfileFlags"       // Profile flags
+	TagICCDeviceAttributes   TagID = "ICC:DeviceAttributes"   // Device attributes
+	TagICCProfileID          TagID = "ICC:ProfileID"          // Profile ID (MD5 hash)
 )
 
-// ICC Color Tags
+// ICC Parsed Tag Values (human-readable names from knownTags)
 const (
-	TagICCMediaWhitePoint    TagID = "ICC:MediaWhitePoint"    // Media white point XYZ
-	TagICCMediaBlackPoint    TagID = "ICC:MediaBlackPoint"    // Media black point XYZ
-	TagICCRedColorant        TagID = "ICC:RedColorant"        // Red matrix column
-	TagICCGreenColorant      TagID = "ICC:GreenColorant"      // Green matrix column
-	TagICCBlueColorant       TagID = "ICC:BlueColorant"       // Blue matrix column
-	TagICCRedTRC             TagID = "ICC:RedTRC"             // Red tone curve
-	TagICCGreenTRC           TagID = "ICC:GreenTRC"           // Green tone curve
-	TagICCBlueTRC            TagID = "ICC:BlueTRC"            // Blue tone curve
-	TagICCGrayTRC            TagID = "ICC:GrayTRC"            // Gray tone curve
-	TagICCLuminance          TagID = "ICC:Luminance"          // Luminance value
-	TagICCChromaticAdaptation TagID = "ICC:ChromaticAdaptation" // Chromatic adaptation
+	TagICCProfileDescription TagID = "ICC:Profile Description" // Profile description
+	TagICCProfileCopyright   TagID = "ICC:Profile Copyright"   // Copyright notice
+	TagICCMediaWhitePoint    TagID = "ICC:Media White Point"   // Media white point XYZ
+	TagICCMediaBlackPoint    TagID = "ICC:Media Black Point"   // Media black point XYZ
+	TagICCChromaticAdaptation TagID = "ICC:Chromatic Adaptation" // Chromatic adaptation matrix
 )
 
-// ICC Device Tags
+// ICC Color Matrix Tags
 const (
-	TagICCDeviceMfgDesc   TagID = "ICC:DeviceMfgDesc"   // Device manufacturer description
-	TagICCDeviceModelDesc TagID = "ICC:DeviceModelDesc" // Device model description
-	TagICCTechnology      TagID = "ICC:Technology"      // Device technology
-	TagICCViewingCondDesc TagID = "ICC:ViewingCondDesc" // Viewing conditions description
-	TagICCMeasurement     TagID = "ICC:Measurement"     // Measurement info
+	TagICCRedMatrixColumn   TagID = "ICC:Red Matrix Column"   // Red matrix column XYZ
+	TagICCGreenMatrixColumn TagID = "ICC:Green Matrix Column" // Green matrix column XYZ
+	TagICCBlueMatrixColumn  TagID = "ICC:Blue Matrix Column"  // Blue matrix column XYZ
+)
+
+// ICC Tone Reproduction Curve Tags
+const (
+	TagICCRedTRC   TagID = "ICC:Red Tone Reproduction Curve"   // Red TRC
+	TagICCGreenTRC TagID = "ICC:Green Tone Reproduction Curve" // Green TRC
+	TagICCBlueTRC  TagID = "ICC:Blue Tone Reproduction Curve"  // Blue TRC
+	TagICCGrayTRC  TagID = "ICC:Gray Tone Reproduction Curve"  // Gray TRC
+)
+
+// ICC Device Description Tags
+const (
+	TagICCDeviceMfgDesc       TagID = "ICC:Device Manufacturer Description" // Device mfg description
+	TagICCDeviceModelDesc     TagID = "ICC:Device Model Description"        // Device model description
+	TagICCTechnology          TagID = "ICC:Technology"                      // Device technology
+	TagICCViewingConditionsDesc TagID = "ICC:Viewing Conditions Description" // Viewing conditions
+	TagICCLuminance           TagID = "ICC:Luminance"                       // Luminance value
+	TagICCMeasurement         TagID = "ICC:Measurement"                     // Measurement info
 )
